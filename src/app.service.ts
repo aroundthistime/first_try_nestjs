53524356054c5e7613d35c4a6611ec07a7a6f264
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-03-04 17:42:03 +0900

    before removing all controllers, services

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World 🤑';
+  }
+  sayHello(): string {
+    return 'Hello from here as well :) ;?';
   }
 }
